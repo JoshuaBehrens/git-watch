00935775a622b3942f57e64762c840b640320335
Joshua Behrens <code@joshua-behrens.de>
2021-01-23 13:48:35 +0100

    Make config dir an option

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,19 @@ int main(int argc, char** argv) {
     namespace fs = std::filesystem;
     namespace bpo = boost::program_options;
 
+    auto configDir(
+        git_watch::path::config_directory()
+            .value_or(fs::current_path().append("config"))
+            .append("git_watch")
+            .generic_string()
+    );
+
     bpo::options_description desc("Allowed arguments and options");
     desc.add_options()
-        ("help", "Show help message");
+        ("help", "Show help message")
+        ("config-dir", bpo::value<std::string>(&configDir)->default_value(configDir), "Set the configuration directory");
+
+    auto configDirPath = fs::path(configDir);
 
     bpo::variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
@@ -20,17 +30,11 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    auto dataDir(
-        git_watch::path::config_directory()
-            .value_or(fs::current_path().append("config"))
-            .append("git_watch")
-    );
-
-    if (!exists(dataDir)) {
-        create_directories(dataDir);
+    if (!exists(configDirPath)) {
+        create_directories(configDirPath);
     }
 
-    std::cout << dataDir.generic_string() << std::endl;
+    std::cout << configDirPath.generic_string() << std::endl;
 
     return 0;
 }
